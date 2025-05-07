b00e18214c67a1746f6ed50bbc4017d76d7e1e55
Onno Kriens <onno.kriens@gmx.de>
2025-05-07 11:31:13 +0200

    Matlab to Python

--- 1_System/Behauviorial_Analysis.py
+++ 1_System/Behauviorial_Analysis.py
@@ -12,13 +12,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Initial values
-f0 = 1e3
-w0 = 2 * np.pi * f0
-Q = 10
+f0 = 1e3 # Resonance frequency in Hz
+w0 = 2 * np.pi * f0 # Angular frequency in rad/s
+Q = 10 # quality factor
 H0 = 1 #play around with this later
 
-#v alues for tf
-b0 = H0 * w0^2;
+#values for tf
+b0 = H0 * w0**2;
 a0 = 1;
 a1 = w0 / Q;
-a2 = w0^2;
+a2 = w0**2;
+
+#Logarithmic frequency axis
+frequencies = np.logspace(2, 4, 1000) # Frequency from 10^2 to 10^4 Hz
+s = 1j * 2 * np.pi * frequencies; # Laplace-Variable s = jω
+
+# Calculation of the transfer function H(s)
+Hs = b0 / (a0 * s**2 + a1 * s + a2)
+
+# Bode Diagram
+fig, axs = plt.subplots(2)
+fig.suptitle("Bode-Diagram of the TF")
+
+axs[0].semilogx(frequencies, 20*np.log10(np.abs(Hs)))
+# axs[0].set_title("Bode-Diagram of the TF22")
+axs[0].set_xlabel("Frequencies in Hz")
+axs[0].set_ylabel("Amplitude in dB")
+axs[0].grid()
+
+axs[1].semilogx(frequencies, np.angle(Hs) * (180/np.pi))
+axs[1].set_xlabel("Frequencies in Hz")
+axs[1].set_ylabel("Phase in degree")
+axs[1].grid()
+
+plt.tight_layout()
+plt.show()
+
+# Pol-Zero Diagram
+poles = np.roots([a0, a1, a2])
+zeros = np.roots([b0])
+
+plt.figure()
+plt.plot(np.real(poles), np.imag(poles), "rx", label="Poles")
+plt.plot(np.real(zeros), np.imag(zeros), "go", label="Zeros")
+plt.grid()
+plt.title("Pole-Zero Diagram")
+plt.xlabel("Real Part")
+plt.ylabel("Imaginary Part")
+plt.legend()
+plt.axis("equal")
+
+print("Poles")
+print(poles)
+
+print("Zeros")
+print(zeros)
