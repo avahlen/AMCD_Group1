9d4687aef399970abf548e00973e259de3cbe780
avahlen <a.von.ahlen@gmx.de>
2025-05-07 09:04:15 +0200

    python simulation starter packet

---
+++ 1_System/Behauviorial_Analysis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  7 08:59:00 2025
+
+@author: raraidos
+"""
+
+#Behauviorial Analysis Biquad Filter
+
+import numpy as np
+import matplotlib.pyplot as plt
